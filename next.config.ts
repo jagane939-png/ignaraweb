8003508fff86581b26b5a0f0eb5022ab3ad0413d
Jagannadham Eerichetty <yentriai@Jagannadhams-Mac-mini.local>
2026-06-27 08:04:59 -0400

    fix: set turbopack root to resolve Vercel build issue

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  experimental: {
+    turbo: {
+      root: __dirname,
+    },
+  },
 };
 
 export default nextConfig;
